fc52b0fb3b7636e017f3f8bc812303ef6c735d19
Whitigol <whitetigerwolfthe1st@gmail.com>
2023-09-18 22:34:50 -0400

    🎉 Useful Links & New Index

--- src/components/Navigation/IndexLink.tsx
+++ src/components/Navigation/IndexLink.tsx
@@ -6,6 +6,7 @@ interface Props {
     title?: string;
     description?: string;
     to?: string;
+    target?: "_blank" | "_self" | "_parent" | "_top";
 }
 
 export default function IndexLink(props: Props) {
@@ -20,7 +21,7 @@ export default function IndexLink(props: Props) {
     return (
         <Link
             to={props.to}
-            target="_self"
+            target={props.target || "_self"}
             className="flex flex-col gap-2 w-full py-3 px-4 text-lg font-bold bg-zinc-500/20 rounded-md hover:bg-zinc-400/20 outline-1 outline-purple-500 hover:outline transition-all ease-linear duration-100"
             style={{
                 textDecoration: "none",
